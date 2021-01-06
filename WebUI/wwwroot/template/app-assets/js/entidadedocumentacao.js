781f7ade10e2810fa6a0e3f19dc464d703a03cdb
eduardovatuva <eduardovatuva@gmail.com>
2021-01-06 06:39:19 +0100

    update

--- WebUI/wwwroot/template/app-assets/js/entidadedocumentacao.js
+++ WebUI/wwwroot/template/app-assets/js/entidadedocumentacao.js
@@ -1,149 +1,130 @@
-﻿$(document).ready(function ()
-{   
-    loadData(); 
+﻿$(document).ready(function () {
+    loadData();
 });
 
 function loadData() {
     $.ajax({
-        url: "/Home/Armazem",
+        url: "/Home/Entidade",
         type: "GET",
         contentType: "application/json;charset=utf-8",
         dataType: "json",
-        success: function(result) {
+        success: function (result) {
             var html = '';
             $.each(result, function (key, item) {
-            
+
                 html += '&lt;tr&gt;';
-                html += '&lt;td&gt;' + item.txtsigla + '&lt;/td&gt;';
-                html += '&lt;td&gt;' + item.txtdesignacao + '&lt;/td&gt;';
-                html += '&lt;td&gt;' + item.cbactivo + '&lt;/td&gt;';
-                html += '&lt;td&gt;' + item.cbWarehouseType + '&lt;/td&gt;';
-                html += '&lt;td&gt;' + item.cbAlertaStockMinimo + '&lt;/td&gt;';
-                html += '&lt;td&gt;' + item.cbPermiteStockNegativo + '&lt;/td&gt;';
-                html += '&lt;td&gt;' + item.cbAlertaStockNegativo + '&lt;/td&gt;';
-                html += '&lt;td&gt;' + item.cbPOS + '&lt;/td&gt;';
-                html += '&lt;td&gt;' + item.cbIN + '&lt;/td&gt;';
-                html += '&lt;td&gt;' + item.cbOUT + '&lt;/td&gt;';
-                html += '&lt;td&gt;' + item.cbPOS + '&lt;/td&gt;';
-                html += '&lt;td&gt;&lt;a href="#" onclick="return getbyID(' + item.txtarmazemid + ')"&gt;Edit&lt;/a&gt; | &lt;a href="#" onclick="Delele(' + txtarmazemid + ')"&gt;Delete&lt;/a&gt;&lt;/td&gt;';
-                html += '&lt;/tr&gt;';
+                html += '&lt;td&gt;' + item.txtnumero + '&lt;/td&gt;';
+                html += '&lt;td&gt;' + item.txtreferencia + '&lt;/td&gt;';
+                html += '&lt;td&gt;' + item.txtnomecomercial + '&lt;/td&gt;';
+                html += '&lt;td&gt;' + item.txtdocumento + '&lt;/td&gt;';
+                html += '&lt;td&gt;' + item.txtentidade + '&lt;/td&gt;';
+                html += '&lt;td&gt;' + item.txtemissao + '&lt;/td&gt;';
+                html += '&lt;td&gt;' + item.txtvalidade + '&lt;/td&gt;';
+                html += '&lt;td&gt;' + item.txtlocal + '&lt;/td&gt;';
                 
+             
+                html += '&lt;td&gt;&lt;a href="#" onclick="return GetbyID(' + item.txtreferencia + ')"&gt;Edit&lt;/a&gt; | &lt;a href="#" onclick="Delele(' + txtreferencia + ')"&gt;Delete&lt;/a&gt;&lt;/td&gt;';
+                html += '&lt;/tr&gt;';
+
             });
             $('.tbody').html(html);
-            
+
         },
-        
-        error: function(errormessage) {
-            
+
+        error: function (errormessage) {
+
             alert(errormessage.responseText);
-           
+
         }
     });
-    
+
 }
-function Salvar()
-{
+function Salvar() {
     var res = validate();
     if (res == false) {
         return false;
     }
-   
+
     var Obj = {
-        Codigo: $('#txtarmazemId').val(),
-        Sigla: $('#txtsigla').val(),
-        Descricao: $('#txtdesignacao').val(),
-        Status: $('#cbactivo').val(),
-        AlertaStockMinimo: $('#cbAlertaStockMinimo').val(),
-        PermiteStockNegativo: $('#cbPermiteStockNegativo').val(),
-        AlertaStockNegativo: $('#cbAlertaStockNegativo').val(),
-        Tipo: $('#cbWarehouseType').val(),
-        EnablePOS: $('#cbPOS').val(),
-        AllowIncome: $('#cbIN').val(),
-        AllowOutcome: $('#cbOUT').val(),
-        IsForRestTipo: $('#isForRest').val(),
-        TablePriceID: $('#tablepriceId').val()
+        Numero: $('#txtnumero').val(),
+        Referencia: $('#txtreferencia').val(),
+        Documento: $('#txtdocumento').val(),
+        Entidade: $('#txtentidade').val(),
+        Emissao: $('#txtemissao').val(),
+        Validade: $('#txtvalidade').val(),
+        Local: $('#txtlocal').val()
+          
+
     };
-    
+
     $.ajax(
         url: "/Home/Salvar",
         data: JSON.stringify(Obj),
         type: "POST",
         contentType: "application/json;charset=utf-8",
         dataType: "json",
-        
-        success: function(result) {     
+
+        success: function (result) {
             loadData();
-            
-            $('#myModal').modal('hide');  
+
+            $('#myModal').modal('hide');
         },
-        
-        error: function(errormessage) {
-            
+
+        error: function (errormessage) {
+
             alert(errormessage.responseText);
-           
+
         }
 
     });
   
-}  
+}
 
 function GetbyID(Id) {
     $('#txtsigla').css('border-color', 'lightgrey');
     $('#txtdesignacao').css('border-color', 'lightgrey');
-   
+
     $.ajax({
-        url: "/Home/GetbyID/"+Id,
+        url: "/Home/GetbyID/" + Id,
         typr: "GET",
         contentType: "application/json;charset=UTF-8",
         dataType: "json",
 
         success: function (result) {
-            $('#txtarmazemId').val(result.Codigo);
-            $('#txtsigla').val(result.Sigla);
-            $('#txtdesignacao').val(result.Descricao);
-            $('#cbactivo').val(result.Status);
-            $('#cbAlertaStockMinimo').val(result.AlertaStockMinimo);
-            $('#cbPermiteStockNegativo').val(result.PermiteStockNegativo);
-            $('#cbAlertaStockNegativo').val(result.AlertaStockNegativo);
-            $('#cbWarehouseType').val(result.Tipo);
-            $('#cbPOS').val(result.EnablePOS);
-            $('#cbIN').val(result.AllowIncome);
-            $('#cbOUT').val(result.AllowOutcome);
-            $('#isForRest').val(result.IsForRestTipo);
-            $('#tablepriceId').val(result.TablePriceID);
+
+            $('#txtnumero').val(result.Numero),
+                $('#txtreferencia').val(result.Referencia),
+                $('#txtdocumento').val(result.Documento),
+                $('#txtentidade').val(result.Entidade),
+                $('#txtemissao').val(result.Emissao),
+                $('#txtvalidade').val(result.Validade),
+                $('#txtlocal').val(result.Local),
+               
             $('#myModal').modal('show');
             $('#btnEditar').show();
             $('#btnSalvar').hide();
         },
-        error: function(errormessage) {
+        error: function (errormessage) {
             alert(errormessage.responseText);
         }
     });
-   
+
     return false;
 }
-function Update()
-{
+function Update() {
     var res = validate();
-    if (res == false)
-    {
+    if (res == false) {
         return false;
-        
+
     }
     var Obj = {
-        Codigo: $('#txtarmazemId').val(),
-        Sigla: $('#txtsigla').val(),
-        Descricao: $('#txtdesignacao').val(),
-        Status: $('#cbactivo').val(),
-        AlertaStockMinimo: $('#cbAlertaStockMinimo').val(),
-        PermiteStockNegativo: $('#cbPermiteStockNegativo').val(),
-        AlertaStockNegativo: $('#cbAlertaStockNegativo').val(),
-        Tipo: $('#cbWarehouseType').val(),
-        EnablePOS: $('#cbPOS').val(),
-        AllowIncome: $('#cbIN').val(),
-        AllowOutcome: $('#cbOUT').val(),
-        IsForRestTipo: $('#isForRest').val(),
-        TablePriceID: $('#tablepriceId').val()
+        Numero: $('#txtnumero').val(),
+        Referencia: $('#txtreferencia').val(),
+        Documento: $('#txtdocumento').val(),
+        Entidade: $('#txtentidade').val(),
+        Emissao: $('#txtemissao').val(),
+        Validade: $('#txtvalidade').val(),
+        Local: $('#txtlocal').val()
     };
     $.ajax({
         url: "/Home/Update",
@@ -151,38 +132,32 @@ function Update()
         type: "POST",
         contentType: "application/json;charset=utf-8",
         dataType: "json",
-        success: function(result) {
+        success: function (result) {
             loadData();
-            $('#myModal').modal('hide');
-            $('#txtarmazemId').val("");
-            $('#txtsigla').val(),
-            $('#txtdesignacao').val(),
-            $('#cbactivo').val(),
-            $('#cbAlertaStockMinimo').val(),
-            $('#cbPermiteStockNegativo').val(),
-            $('#cbAlertaStockNegativo').val(),
-            $('#cbWarehouseType').val(),
-            $('#cbPOS').val(),
-            $('#cbIN').val(),
-            $('#cbOUT').val(),
-            $('#isForRest').val(),
-            $('#tablepriceId').val()
-            
+               $('#myModal').modal('hide');
+               $('#txtnumero').val(),
+                $('#txtreferencia').val(),
+                $('#txtdocumento').val(),
+                $('#txtentidade').val(),
+                $('#txtemissao').val(),
+                $('#txtvalidade').val(),
+                $('#txtlocal').val()
+           
         },
-       
-        error: function(errormessage) {
-            
+
+        error: function (errormessage) {
+
             alert(errormessage.responseText);
-            
+
         }
     });
- 
+
 }
 
 function Delele(Id) {
     var ans = confirm("Tem certeza que pretende eliminar os dados? ");
     if (ans) {
-      $.ajax({
+        $.ajax({
             url: "/Home/Delete/" + Id,
             type: "POST",
             contentType: "application/json;charset=UTF-8",
@@ -201,48 +176,90 @@ function Delele(Id) {
 
 }
 function ClearFields() {
-    $('#txtarmazemId').val("");
-    $('#txtsigla').val("");
-    $('#txtdesignacao').val("");
-    $('#cbactivo').val("");
-    $('#cbAlertaStockMinimo').val("");
-    $('#cbPermiteStockNegativo').val("");
-    $('#cbAlertaStockNegativo').val("");
-    $('#cbWarehouseType').val("");
-    $('#cbPOS').val("");
-    $('#cbIN').val("");
-    $('#cbOUT').val("");
-    $('#isForRest').val("");
-    $('#tablepriceId').val("");
+    $('#txtnumero').val(""),
+        $('#txtreferencia').val(""),
+        $('#txtdocumento').val(""),
+        $('#txtentidade').val(""),
+        $('#txtemissao').val(""),
+        $('#txtvalidade').val(""),
+        $('#txtlocal').val("")
+        
+
     $('#myModal').modal('show');
     $('#btnEditar').hide();
     $('#btnSalvar').show();
     $('#btnUpdate').hide();
-   
+
     $('#txtsigla').css('border-color', 'lightgrey');
     $('#txtdesignacao').css('border-color', 'lightgrey');
-   
-    
+    $('#estado').css('border-color', 'lightgrey');
+
+
+
 }
 
 function validate() {
     var isValid = true;
-    if ($('#txtsigla').val().trim() == "") {
-        $('#txtsigla').css('border-color', 'Red');
-        
-        isValid = false;  
+    if ($('#txtnumero').val().trim() == "") {
+        $('#txtnumero').css('border-color', 'Red');
+
+        isValid = false;
     }
     else {
-        
-        $('#txtsigla').css('border-color', 'lightgrey');
+
+        $('#txtnumero').css('border-color', 'lightgrey');
     }
-    if ($('#txtdesignacao').val().trim() == "") {
-        $('#txtdesignacao').css('border-color', 'Red');
+    if ($('#txtreferencia').val().trim() == "") {
+        $('#txtreferencia').css('border-color', 'Red');
         isValid = false;
+    }
+    else {
+
+        $('#txtreferencia').css('border-color', 'lightgrey');
+    }
+    if ($('#txtdocumento').val().trim() == "") {
+        $('#txtdocumento').css('border-color', 'Red');
+        isValid = false;
+    }
+    else {
+
+        $('#txtdocumento').css('border-color', 'lightgrey');
+    }
+    if ($('#txtentidade').val().trim() == "") {
+        $('#txtentidade').css('border-color', 'Red');
+
+        isValid = false;
+    }
+    else {
+
+        $('#txtentidade').css('border-color', 'lightgrey');
+    }
+    if ($('#txtemissao').val().trim() == "") {
+        $('#txtemissao').css('border-color', 'Red');
+        isValid = false;
+    }
+    else {
+
+        $('#txtemissao').css('border-color', 'lightgrey');
+    }
+    if ($('#txtvalidade').val().trim() == "") {
+        $('#txtvalidade').css('border-color', 'Red');
+        isValid = false;
+    }
+    else {
+
+        $('#txtvalidade').css('border-color', 'lightgrey');
+    }
+    if ($('#txtlocal').val().trim() == "") {
+        $('#txtlocal').css('border-color', 'Red');
+
+        isValid = false;
+    }
+    else {
+
+        $('#txtlocal').css('border-color', 'lightgrey');
     }
     
     return isValid;
-    
-}
 
-​
+}
--- WebUI/wwwroot/template/app-assets/js/moeda.js
+++ WebUI/wwwroot/template/app-assets/js/moeda.js
@@ -1,149 +1,113 @@
-﻿$(document).ready(function ()
-{   
-    loadData(); 
+﻿$(document).ready(function () {
+    loadData();
 });
 
 function loadData() {
     $.ajax({
-        url: "/Home/Armazem",
+        url: "/Home/Moeda",
         type: "GET",
         contentType: "application/json;charset=utf-8",
         dataType: "json",
-        success: function(result) {
+        success: function (result) {
             var html = '';
             $.each(result, function (key, item) {
-            
+
                 html += '&lt;tr&gt;';
+                html += '&lt;td&gt;' + item.txtreferencia + '&lt;/td&gt;';
                 html += '&lt;td&gt;' + item.txtsigla + '&lt;/td&gt;';
                 html += '&lt;td&gt;' + item.txtdesignacao + '&lt;/td&gt;';
-                html += '&lt;td&gt;' + item.cbactivo + '&lt;/td&gt;';
-                html += '&lt;td&gt;' + item.cbWarehouseType + '&lt;/td&gt;';
-                html += '&lt;td&gt;' + item.cbAlertaStockMinimo + '&lt;/td&gt;';
-                html += '&lt;td&gt;' + item.cbPermiteStockNegativo + '&lt;/td&gt;';
-                html += '&lt;td&gt;' + item.cbAlertaStockNegativo + '&lt;/td&gt;';
-                html += '&lt;td&gt;' + item.cbPOS + '&lt;/td&gt;';
-                html += '&lt;td&gt;' + item.cbIN + '&lt;/td&gt;';
-                html += '&lt;td&gt;' + item.cbOUT + '&lt;/td&gt;';
-                html += '&lt;td&gt;' + item.cbPOS + '&lt;/td&gt;';
-                html += '&lt;td&gt;&lt;a href="#" onclick="return getbyID(' + item.txtarmazemid + ')"&gt;Edit&lt;/a&gt; | &lt;a href="#" onclick="Delele(' + txtarmazemid + ')"&gt;Delete&lt;/a&gt;&lt;/td&gt;';
+               
+
+                html += '&lt;td&gt;&lt;a href="#" onclick="return getbyID(' + item.txtreferencia + ')"&gt;Edit&lt;/a&gt; | &lt;a href="#" onclick="Delele(' + txtreferencia + ')"&gt;Delete&lt;/a&gt;&lt;/td&gt;';
                 html += '&lt;/tr&gt;';
-                
+
             });
             $('.tbody').html(html);
-            
+
         },
-        
-        error: function(errormessage) {
-            
+
+        error: function (errormessage) {
+
             alert(errormessage.responseText);
-           
+
         }
     });
-    
+
 }
-function Salvar()
-{
+function Salvar() {
     var res = validate();
     if (res == false) {
         return false;
     }
-   
+
     var Obj = {
-        Codigo: $('#txtarmazemId').val(),
+        Codigo: $('#txtreferencia').val(),
         Sigla: $('#txtsigla').val(),
-        Descricao: $('#txtdesignacao').val(),
-        Status: $('#cbactivo').val(),
-        AlertaStockMinimo: $('#cbAlertaStockMinimo').val(),
-        PermiteStockNegativo: $('#cbPermiteStockNegativo').val(),
-        AlertaStockNegativo: $('#cbAlertaStockNegativo').val(),
-        Tipo: $('#cbWarehouseType').val(),
-        EnablePOS: $('#cbPOS').val(),
-        AllowIncome: $('#cbIN').val(),
-        AllowOutcome: $('#cbOUT').val(),
-        IsForRestTipo: $('#isForRest').val(),
-        TablePriceID: $('#tablepriceId').val()
+        Designacao: $('#txtdesignacao').val(),
+      
+
     };
-    
+
     $.ajax(
         url: "/Home/Salvar",
         data: JSON.stringify(Obj),
         type: "POST",
         contentType: "application/json;charset=utf-8",
         dataType: "json",
-        
-        success: function(result) {     
+
+        success: function (result) {
             loadData();
-            
-            $('#myModal').modal('hide');  
+
+            $('#myModal').modal('hide');
         },
-        
-        error: function(errormessage) {
-            
+
+        error: function (errormessage) {
+
             alert(errormessage.responseText);
-           
+
         }
 
     });
   
-}  
+}
 
 function GetbyID(Id) {
     $('#txtsigla').css('border-color', 'lightgrey');
     $('#txtdesignacao').css('border-color', 'lightgrey');
-   
+
     $.ajax({
-        url: "/Home/GetbyID/"+Id,
+        url: "/Home/GetbyID/" + Id,
         typr: "GET",
         contentType: "application/json;charset=UTF-8",
         dataType: "json",
 
         success: function (result) {
-            $('#txtarmazemId').val(result.Codigo);
+            $('#txtreferencia').val(result.Codigo);
             $('#txtsigla').val(result.Sigla);
             $('#txtdesignacao').val(result.Descricao);
-            $('#cbactivo').val(result.Status);
-            $('#cbAlertaStockMinimo').val(result.AlertaStockMinimo);
-            $('#cbPermiteStockNegativo').val(result.PermiteStockNegativo);
-            $('#cbAlertaStockNegativo').val(result.AlertaStockNegativo);
-            $('#cbWarehouseType').val(result.Tipo);
-            $('#cbPOS').val(result.EnablePOS);
-            $('#cbIN').val(result.AllowIncome);
-            $('#cbOUT').val(result.AllowOutcome);
-            $('#isForRest').val(result.IsForRestTipo);
-            $('#tablepriceId').val(result.TablePriceID);
+           
             $('#myModal').modal('show');
             $('#btnEditar').show();
             $('#btnSalvar').hide();
         },
-        error: function(errormessage) {
+        error: function (errormessage) {
             alert(errormessage.responseText);
         }
     });
-   
+
     return false;
 }
-function Update()
-{
+function Update() {
     var res = validate();
-    if (res == false)
-    {
+    if (res == false) {
         return false;
-        
+
     }
     var Obj = {
-        Codigo: $('#txtarmazemId').val(),
+        Codigo: $('#txtreferencia').val(),
         Sigla: $('#txtsigla').val(),
         Descricao: $('#txtdesignacao').val(),
-        Status: $('#cbactivo').val(),
-        AlertaStockMinimo: $('#cbAlertaStockMinimo').val(),
-        PermiteStockNegativo: $('#cbPermiteStockNegativo').val(),
-        AlertaStockNegativo: $('#cbAlertaStockNegativo').val(),
-        Tipo: $('#cbWarehouseType').val(),
-        EnablePOS: $('#cbPOS').val(),
-        AllowIncome: $('#cbIN').val(),
-        AllowOutcome: $('#cbOUT').val(),
-        IsForRestTipo: $('#isForRest').val(),
-        TablePriceID: $('#tablepriceId').val()
+    
     };
     $.ajax({
         url: "/Home/Update",
@@ -151,38 +115,31 @@ function Update()
         type: "POST",
         contentType: "application/json;charset=utf-8",
         dataType: "json",
-        success: function(result) {
+        success: function (result) {
             loadData();
             $('#myModal').modal('hide');
-            $('#txtarmazemId').val("");
-            $('#txtsigla').val(),
-            $('#txtdesignacao').val(),
-            $('#cbactivo').val(),
-            $('#cbAlertaStockMinimo').val(),
-            $('#cbPermiteStockNegativo').val(),
-            $('#cbAlertaStockNegativo').val(),
-            $('#cbWarehouseType').val(),
-            $('#cbPOS').val(),
-            $('#cbIN').val(),
-            $('#cbOUT').val(),
-            $('#isForRest').val(),
-            $('#tablepriceId').val()
-            
+            $('#txtreferencia').val(),
+                $('#txtsigla').val(),
+                $('#txtdesignacao').val(),
+                
+               
+
+
         },
-       
-        error: function(errormessage) {
-            
+
+        error: function (errormessage) {
+
             alert(errormessage.responseText);
-            
+
         }
     });
- 
+
 }
 
 function Delele(Id) {
     var ans = confirm("Tem certeza que pretende eliminar os dados? ");
     if (ans) {
-      $.ajax({
+        $.ajax({
             url: "/Home/Delete/" + Id,
             type: "POST",
             contentType: "application/json;charset=UTF-8",
@@ -201,48 +158,47 @@ function Delele(Id) {
 
 }
 function ClearFields() {
-    $('#txtarmazemId').val("");
+    $('#txtreferencia').val("");
     $('#txtsigla').val("");
     $('#txtdesignacao').val("");
-    $('#cbactivo').val("");
-    $('#cbAlertaStockMinimo').val("");
-    $('#cbPermiteStockNegativo').val("");
-    $('#cbAlertaStockNegativo').val("");
-    $('#cbWarehouseType').val("");
-    $('#cbPOS').val("");
-    $('#cbIN').val("");
-    $('#cbOUT').val("");
-    $('#isForRest').val("");
-    $('#tablepriceId').val("");
+ 
+
+
     $('#myModal').modal('show');
     $('#btnEditar').hide();
     $('#btnSalvar').show();
     $('#btnUpdate').hide();
-   
+
     $('#txtsigla').css('border-color', 'lightgrey');
     $('#txtdesignacao').css('border-color', 'lightgrey');
-   
-    
+    $('#estado').css('border-color', 'lightgrey');
+
+
+
 }
 
 function validate() {
     var isValid = true;
     if ($('#txtsigla').val().trim() == "") {
         $('#txtsigla').css('border-color', 'Red');
-        
-        isValid = false;  
+
+        isValid = false;
     }
     else {
-        
+
         $('#txtsigla').css('border-color', 'lightgrey');
     }
     if ($('#txtdesignacao').val().trim() == "") {
         $('#txtdesignacao').css('border-color', 'Red');
         isValid = false;
     }
+    else {
+
+        $('#txtdesignacao').css('border-color', 'lightgrey');
+    }
     
     return isValid;
-    
+
 }
 
-​
+
